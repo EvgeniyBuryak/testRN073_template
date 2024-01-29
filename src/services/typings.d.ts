e9a311e75ffdfb37db258a56e3ea0d3f1a423bec
Evgeniy Buryak <buryakone@gmail.com>
2024-01-29 14:28:19 +0700

    add types

---
+++ src/services/typings.d.ts
@@ -0,0 +1,20 @@
+type TOfferSpecial = {
+    id: number,
+    url: string,
+    title: string,
+    text: string,
+    mobile: string,
+    mobileApp: null,
+    desktop: string,
+    video: null,
+    metadata: null,
+};
+
+type TFrontTest = {
+	id: number,
+	user: null,
+	order: null,
+	published_at: string,
+	created_at: string,
+	updated_at: string,
+};
